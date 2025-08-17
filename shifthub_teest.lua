365711d19fcf8113558d23fcedb333c5cf5d9b4a
Shifthub <rumbadonquixote278@gmail.com>
2025-08-17 14:42:49 -0300

    Update shifthub_teest.lua

--- shifthub_teest.lua
+++ shifthub_teest.lua
@@ -62,7 +62,7 @@ keyTab:CreateButton({
         end
 
         -- SUBSTITUA ESTA URL PELA SUA URL ATUAL DO NGROK
-        local ngrokUrl = "https://5752298027ba.ngrok-free.app"
+        local ngrokUrl = "https://b46a5ef2d50d.ngrok-free.app"
         local url = ngrokUrl .. "/validate"
 
         local response = request({
@@ -241,5 +241,6 @@ function openMainWindow()
     mainWindow.Visible = true
     playSound(openSoundId)
 end
+
 
 
